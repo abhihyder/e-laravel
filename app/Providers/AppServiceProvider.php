771b95a3a94311d6e97288c46294a99a34e94330
abhi-hyder <abhihyder7@gmail.com>
2020-04-03 19:13:41 +0600

    added view share in AppServiceProvider and header

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Providers;
-
+use App\Models\Category;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Facades\View;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Builder::defaultStringLength(191); // Update defaultStringLength
+
+
+        $categories= Category::select('name', 'slug')->where('category_id', null)->get();
+        View::share('categories', $categories);
     }
 
 }
